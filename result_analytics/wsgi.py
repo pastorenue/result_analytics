c91146854515b3cffb983141705aac481ae17476
Pastor, Emmanuel <pastorenuel@gmail.com>
2017-09-16 20:45:25 +0100

    modified the settings file

--- result_analytics/wsgi.py
+++ result_analytics/wsgi.py
@@ -14,4 +14,4 @@ from whitenoise.django import DjangoWhiteNoise
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "result_analytics.settings")
 
 application = get_wsgi_application()
-#application = DjangoWhiteNoise(application)
+application = DjangoWhiteNoise(application)
