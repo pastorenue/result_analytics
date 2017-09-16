db37cd23f582083ba5ab0a66c02092f519ee3975
Pastor, Emmanuel <pastorenuel@gmail.com>
2017-09-16 18:56:38 +0100

    modified the settings file

--- result_analytics/settings.py
+++ result_analytics/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'livereload',
     'django.contrib.staticfiles',
     'analyzer',
     'assignments',
